b3e70aecb2f2a8b45cbe64207c7b64117e5a015d
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-06-01 09:38:53 +0300

    File I/O Lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
-def write_file(file_name, file_content):
-    pass
+from pathlib import Path
 
-def append_file(file_name, append_content):
-    pass
+def write_file(file_name, file_content):
+    file_path = Path(file_name).with_suffix('.txt')
+    with open(file_path, 'w') as file:
+        file.write(file_content)
+
+
+def append_file(file_name, file_content):
+   file_path = Path(file_name).with_suffix('.txt')
+   with open(file_path, 'a') as file:
+        file.write(file_content)
 
 def read_file(file_name):
-    pass
+    file_path = Path(file_name).with_suffix('.txt')
+    with open(file_path, 'r') as file:
+        return file.read()
